3a1872731ed667992a74ff82ac54d2d546143f4d
Alexander Brese <admin@alexanderbrese.com>
2021-06-02 08:32:49 +0200

    feat: refactor

--- event_handle/src/lib.rs
+++ event_handle/src/lib.rs
@@ -1,51 +1,39 @@
 pub mod event_handle {
     use std::{error::Error, path::PathBuf};
 
-    use store::store::Store;
     use diff::diff::LineDifference;
     use notify::DebouncedEvent;
+    use store::store::Store;
 
     pub struct EventHandle {
-        store: Store
+        store: Store,
     }
 
     impl EventHandle {
         pub fn new(store: Store) -> EventHandle {
-            EventHandle{
-                store
-            }
+            EventHandle { store }
         }
 
         pub fn handle(&self, event: DebouncedEvent) -> Result<(), String> {
-            self.on_modification(&event)?;
-            self.on_removal(&event)
+            let path = self.to_path(&event)?;
+            if self.is_not_directory(path) {
+                self.on_modification(&event);
+                self.on_removal(&event);
+            }
+            Ok(())
         }
 
-        fn on_modification(&self, event: &DebouncedEvent) -> Result<(), String> {
+        fn on_modification(&self, event: &DebouncedEvent) {
             //println!("{:?}", event);
-            let path = self.to_path(&event)?;
             if self.is_modification(&event) {
-                if self.is_directory(path) {
-                    self.on_dir_change(&event);
-                } else {
-                    self.on_file_change(&event);
-                }
+                self.on_file_change(&event);
             }
-
-            Ok(())
         }
 
-        fn on_removal(&self, event: &DebouncedEvent) -> Result<(), String> {
-            let path = self.to_path(&event)?;
+        fn on_removal(&self, event: &DebouncedEvent) {
             if self.is_removed(&event) {
-                if self.is_directory(path) {
-                    self.on_dir_remove(&event);
-                } else {
-                    self.on_file_remove(&event);
-                }
+                self.on_file_remove(&event);
             }
-
-            Ok(())
         }
 
         fn to_path(&self, event: &DebouncedEvent) -> Result<PathBuf, String> {
@@ -64,27 +52,19 @@ pub mod event_handle {
         fn on_file_remove(&self, event: &DebouncedEvent) {
             println!("File removed: {:?}", event);
         }
-    
-        fn on_dir_change(&self, event: &DebouncedEvent) {
-            println!("Directory modified: {:?}", event);
-        }
 
-        fn on_dir_remove(&self, event: &DebouncedEvent) {
-            println!("Directory removed: {:?}", event);
-        }
-    
         fn is_modification(&self, event: &DebouncedEvent) -> bool {
             if let DebouncedEvent::Write(_) = event {
-                return true
+                return true;
             }
             false
         }
-        fn is_directory(&self, path: PathBuf) -> bool {
-            path.is_dir()
+        fn is_not_directory(&self, path: PathBuf) -> bool {
+            !path.is_dir()
         }
         fn is_removed(&self, event: &DebouncedEvent) -> bool {
             if let DebouncedEvent::Remove(_) = event {
-                return true
+                return true;
             }
             false
         }
