b90cfdc37a06b9a5cf33f762318c303c7fb6147f
Alexander Brese <admin@alexanderbrese.com>
2021-06-02 14:37:48 +0200

    feat: refactor event handle

--- event_handle/src/lib.rs
+++ event_handle/src/lib.rs
@@ -1,5 +1,5 @@
 pub mod event_handle {
-    use std::{error::Error, path::PathBuf};
+    use std::path::PathBuf;
 
     use store::store::Store;
     use diff::diff::LineDifference;
@@ -17,35 +17,24 @@ pub mod event_handle {
         }
 
         pub fn handle(&self, event: DebouncedEvent) -> Result<(), String> {
-            self.on_modification(&event)?;
-            self.on_removal(&event)
+            let path = self.to_path(&event)?;
+            if path.is_file() {
+                self.on_modification(&event);
+                self.on_removal(&event);
+            }
+            Ok(())
         }
 
-        fn on_modification(&self, event: &DebouncedEvent) -> Result<(), String> {
-            //println!("{:?}", event);
-            let path = self.to_path(&event)?;
+        fn on_modification(&self, event: &DebouncedEvent) {
             if self.is_modification(&event) {
-                if self.is_directory(path) {
-                    self.on_dir_change(&event);
-                } else {
-                    self.on_file_change(&event);
-                }
+                self.on_file_change(&event);
             }
-
-            Ok(())
         }
 
-        fn on_removal(&self, event: &DebouncedEvent) -> Result<(), String> {
-            let path = self.to_path(&event)?;
+        fn on_removal(&self, event: &DebouncedEvent) {
             if self.is_removed(&event) {
-                if self.is_directory(path) {
-                    self.on_dir_remove(&event);
-                } else {
-                    self.on_file_remove(&event);
-                }
+                self.on_file_remove(&event);
             }
-
-            Ok(())
         }
 
         fn to_path(&self, event: &DebouncedEvent) -> Result<PathBuf, String> {
@@ -53,7 +42,7 @@ pub mod event_handle {
                 DebouncedEvent::Write(p) => Ok(p.clone()),
                 DebouncedEvent::Remove(p) => Ok(p.clone()),
                 DebouncedEvent::Error(e, _) => Err(e.to_string()),
-                _ => Err(String::from("Event is not handled yet")),
+                _ => Err(format!("Event is not handled yet: {:?}", event)),
             }
         }
 
@@ -78,9 +67,6 @@ pub mod event_handle {
                 return true
             }
             false
-        }
-        fn is_directory(&self, path: PathBuf) -> bool {
-            path.is_dir()
         }
         fn is_removed(&self, event: &DebouncedEvent) -> bool {
             if let DebouncedEvent::Remove(_) = event {
