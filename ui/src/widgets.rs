e41481ae1bc8af05e22ddf42b700762cb9363eb0
wolfbiker1 <be631wil@htwg-konstanz.de>
2021-06-24 06:43:27 +0200

    feat: hits of code mock

--- ui/src/widgets.rs
+++ ui/src/widgets.rs
@@ -26,10 +26,11 @@ pub mod widgets {
             let tabs = Tabs::new(titles)
                 .block(
                     Block::default()
+                        .border_style(Style::default().fg(Color::Rgb(129,161,193)))
                         .borders(Borders::ALL)
                         .title(self.config.title.as_str()),
                 )
-                .highlight_style(Style::default().fg(Color::Rgb(136,192,208)))
+                .highlight_style(Style::default().fg(Color::Rgb(235,203,139)))
                 .select(self.state.tabs.index);
             f.render_widget(tabs, chunks[0]);
             self.draw_tab(f, chunks[1])
@@ -70,7 +71,7 @@ pub mod widgets {
                 )
                 .margin(1)
                 .split(area);
-            let block = Block::default().borders(Borders::ALL).title("Differences");
+            let block = Block::default().border_style(Style::default().fg(Color::Rgb(136,192,208))).borders(Borders::ALL).title("Differences");
             // let block = block.border_type(BorderType::Thick);
             let text: Vec<Spans> = self.state.processed_diffs.clone();
             let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
@@ -134,7 +135,7 @@ pub mod widgets {
                 Span::from("(q) "),
                 Span::styled("Quit", Style::default().add_modifier(Modifier::BOLD)),
             ]);
-            let block = Block::default().borders(Borders::ALL).title("Shortcuts");
+            let block = Block::default().border_style(Style::default().fg(Color::Rgb(136,192,208))).borders(Borders::ALL).title("Shortcuts");
             let mut text: Vec<Spans> = Vec::new();
 
             // append all
@@ -181,14 +182,14 @@ pub mod widgets {
                         .map(|i| ListItem::new(vec![Spans::from(Span::raw(i.as_str()))]))
                         .collect();
                     let mut snapshots = List::new(snapshots)
-                        .highlight_style(Style::default().add_modifier(Modifier::BOLD))
-                        .highlight_symbol("►");
+                        .highlight_symbol("►")
+                        .highlight_style(Style::default().add_modifier(Modifier::BOLD).fg(Color::Rgb(235,203,139)));
                     if self.state.pane_ptr == 1 {
                         snapshots = snapshots.block(
                             Block::default()
                                 .borders(Borders::ALL)
                                 .border_style(Style::default().fg(Color::Rgb(129,161,193)))
-                                .title(Span::styled("Available Snapshot", Style::default().fg(Color::Rgb(94,129,172))))
+                                .title(Span::styled("Available Snapshot", Style::default().fg(Color::Rgb(136,192,208))))
                         );
                     } else {
                         snapshots = snapshots.block(
@@ -220,8 +221,9 @@ pub mod widgets {
                     .collect();
 
                 let mut filenames = List::new(filenames)
-                    .highlight_style(Style::default().add_modifier(Modifier::BOLD))
-                    .highlight_symbol("►");
+                    .highlight_symbol("►")
+                    .highlight_style(Style::default().add_modifier(Modifier::BOLD).fg(Color::Rgb(235,203,139)));
+                
                 if self.state.pane_ptr == 1 {
                     filenames = filenames.block(
                         Block::default()
@@ -232,7 +234,7 @@ pub mod widgets {
                     );
                 } else {
                     filenames =
-                        filenames.block(Block::default().border_style(Style::default().fg(Color::Rgb(129,161,193))).borders(Borders::ALL).title(Span::styled("Filename", Style::default().fg(Color::Rgb(94,129,172)))));
+                        filenames.block(Block::default().border_style(Style::default().fg(Color::Rgb(136,192,208))).borders(Borders::ALL).title(Span::styled("Filename", Style::default().fg(Color::Rgb(94,129,172)))));
                 }
                 f.render_stateful_widget(filenames, chunks[1], &mut self.state.filenames.state);
             }
@@ -240,15 +242,110 @@ pub mod widgets {
                 let x_labels = vec![];
                 let datasets = vec![
                     Dataset::default()
-                        .name("Legend1")
-                        .marker(symbols::Marker::Dot)
-                        .style(Style::default().fg(Color::Cyan))
-                        .data(&[(34.4, 34.3)]),
-                    Dataset::default()
-                        .name("Legend2")
-                        .marker(symbols::Marker::Dot)
-                        .style(Style::default().fg(Color::Yellow))
-                        .data(&[(34.4, 34.3)]),
+                        .name("Hits of Code")
+                        .marker(symbols::Marker::Braille)
+                        .style(Style::default().fg(Color::Rgb(235,203,139)))
+                        .data(&[(0.0, 0.0) ,
+                        (1.0, 1.0) ,
+                        (2.0, 2.0) ,
+                        (3.0, 3.0) ,
+                        (4.0, 4.0) ,
+                        (5.0, 5.0) ,
+                        (6.0, 6.0) ,
+                        (7.0, 7.0) ,
+                        (8.0, 8.0) ,
+                        (9.0, 9.0) ,
+                        (10.0, 10.0) ,
+                        (11.0, 11.0) ,
+                        (12.0, 12.0) ,
+                        (13.0, 13.0) ,
+                        (14.0, 14.0) ,
+                        (15.0, 15.0) ,
+                        (16.0, 16.0) ,
+                        (17.0, 17.0) ,
+                        (18.0, 18.0) ,
+                        (19.0, 19.0) ,
+                        (20.0, 20.0) ,
+                        (21.0, 21.0) ,
+                        (22.0, 22.0) ,
+                        (23.0, 23.0) ,
+                        (24.0, 24.0) ,
+                        (25.0, 25.0) ,
+                        (26.0, 26.0) ,
+                        (27.0, 27.0) ,
+                        (28.0, 28.0) ,
+                        (29.0, 29.0) ,
+                        (30.0, 30.0) ,
+                        (31.0, 31.0) ,
+                        (32.0, 32.0) ,
+                        (33.0, 33.0) ,
+                        (34.0, 34.0) ,
+                        (35.0, 35.0) ,
+                        (36.0, 36.0) ,
+                        (37.0, 37.0) ,
+                        (38.0, 38.0) ,
+                        (39.0, 39.0) ,
+                        (40.0, 40.0) ,
+                        (41.0, 41.0) ,
+                        (42.0, 42.0) ,
+                        (43.0, 43.0) ,
+                        (44.0, 44.0) ,
+                        (45.0, 45.0) ,
+                        (46.0, 46.0) ,
+                        (47.0, 47.0) ,
+                        (48.0, 48.0) ,
+                        (49.0, 49.0) ,
+                        (50.0, 50.0) ,
+                        (51.0, 51.0) ,
+                        (52.0, 52.0) ,
+                        (53.0, 53.0) ,
+                        (54.0, 54.0) ,
+                        (55.0, 55.0) ,
+                        (56.0, 56.0) ,
+                        (57.0, 57.0) ,
+                        (58.0, 58.0) ,
+                        (59.0, 59.0) ,
+                        (60.0, 60.0) ,
+                        (61.0, 61.0) ,
+                        (62.0, 62.0) ,
+                        (63.0, 63.0) ,
+                        (64.0, 64.0) ,
+                        (65.0, 65.0) ,
+                        (66.0, 66.0) ,
+                        (67.0, 67.0) ,
+                        (68.0, 68.0) ,
+                        (69.0, 69.0) ,
+                        (70.0, 70.0) ,
+                        (71.0, 71.0) ,
+                        (72.0, 72.0) ,
+                        (73.0, 73.0) ,
+                        (74.0, 74.0) ,
+                        (75.0, 75.0) ,
+                        (76.0, 76.0) ,
+                        (77.0, 77.0) ,
+                        (78.0, 78.0) ,
+                        (79.0, 79.0) ,
+                        (80.0, 80.0) ,
+                        (81.0, 81.0) ,
+                        (82.0, 82.0) ,
+                        (83.0, 83.0) ,
+                        (84.0, 84.0) ,
+                        (85.0, 85.0) ,
+                        (86.0, 86.0) ,
+                        (87.0, 87.0) ,
+                        (88.0, 88.0) ,
+                        (89.0, 89.0) ,
+                        (90.0, 90.0) ,
+                        (91.0, 91.0) ,
+                        (92.0, 92.0) ,
+                        (93.0, 93.0) ,
+                        (94.0, 94.0) ,
+                        (95.0, 95.0) ,
+                        (96.0, 96.0) ,
+                        (97.0, 97.0) ,
+                        (98.0, 98.0) ,
+                        (99.0, 99.0) ,
+                        ]),
                 ];
                 let chart = Chart::new(datasets)
                     .block(
@@ -256,7 +353,7 @@ pub mod widgets {
                             .title(Span::styled(
                                 "Hits-Of-Code",
                                 Style::default()
-                                    .fg(Color::Rgb(94,129,172))
+                                    .fg(Color::Rgb(136,192,208))
                                     // .add_modifier(Modifier::BOLD),
                             ))
                             .border_style(Style::default().fg(Color::Rgb(129,161,193)))
@@ -266,12 +363,12 @@ pub mod widgets {
                         Axis::default()
                             .title("Date")
                             .style(Style::default().fg(Color::Gray))
-                            // .bounds(ui.signals.window)
+                            .bounds([0.0, 100.0])
                             .labels(x_labels),
                     )
                     .y_axis(
                         Axis::default()
-                            .title("Hits-of-code * 10000")
+                            // .title("Hits-of-code * 10000")
                             .style(Style::default().fg(Color::Gray))
                             .bounds([0.0, 100.0])
                             .labels(vec![
